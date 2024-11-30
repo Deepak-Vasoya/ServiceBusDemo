051274542819c43719d5b3cf6bd12599c0db840c
Deepak <DVasoya2035@conestogac.on.ca>
2024-11-30 01:42:53 -0500

    Created ServiceBus, Class, Interface and Repositories.

---
+++ ServiceBusDemo/Models/CarDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceBusDemo.Models
+{
+    public class CarDetails
+    {
+        public int CarId { get; set; }
+        public string CarModel { get; set; }
+        public string Price { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ ServiceBusDemo/Repositories/ServiceBus.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text.Json;
+using System.Text;
+using ServiceBusDemo.Models;
+using Microsoft.Azure.ServiceBus;
+namespace ServiceBusDemo.Repositories
+{
+    public class ServiceBus : IServiceBus
+    {
+        private readonly IConfiguration _configuration;
+        public ServiceBus(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendMessageAsync(CarDetails carDetails)
+        {
+            IQueueClient client = new QueueClient(_configuration["AzureServiceBusConnectionString"], _configuration["QueueName"]);
+            //Serialize car details object
+            var messageBody = JsonSerializer.Serialize(carDetails);
+            //Set content type and Guid
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody))
+            {
+                MessageId = Guid.NewGuid().ToString(),
+                ContentType = "application/json"
+            };
+            await client.SendAsync(message);
+        }
+    }
+}
